8017b358871cfdd46c4a36154b07978d4b453e71
Hendril Mendes <hendrilmendes2015@gmail.com>
2024-04-15 17:17:02 -0400

    Updates

--- main.dart.js
+++ main.dart.js
@@ -23175,7 +23175,7 @@ A.ac6.prototype={
 $1(a){var s=A.e0().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/04817c99c9fd4956f27505204f7e344335810aed/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/e76c956498841e1ab458577d3892003e553e4f3c/":s)+a},
 $S:60}
 A.acj.prototype={
 $1(a){this.a.remove()
